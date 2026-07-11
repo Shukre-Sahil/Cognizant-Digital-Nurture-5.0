347ebf7e0d4d144daeb3d49da2e2dc326776d366
Sahil Shukre <sahil.shukre@gmail.com>
2026-07-11 11:56:26 +0530

    Add files via upload

---
+++ Week2/Exercise2 - Spring REST using Spring Boot3/orm-learn/table-creation.sql
@@ -0,0 +1,12 @@
+create database ormlearn;
+use ormlearn;
+
+CREATE TABLE country (
+    co_code VARCHAR(2) PRIMARY KEY,
+    co_name VARCHAR(50)
+);
+
+INSERT INTO country VALUES ('IN', 'India');
+INSERT INTO country VALUES ('US', 'United States of America');
+
+SELECT * FROM country;
